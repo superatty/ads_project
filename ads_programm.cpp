11a1f69e03b6d006ee4258a870b86ad82c1b3e67
Atalay Donat <atalaydonatt@gmail.com>
2023-07-12 22:54:23 +0200

    First implementation of LogLinearRMQ

--- ads_programm.cpp
+++ ads_programm.cpp
@@ -4,6 +4,7 @@
 #include <cstdint>
 #include <string>
 #include <stdexcept>
+#include <cmath>
 
 using namespace std;
 
@@ -19,9 +20,9 @@ class NaiveRMQ : public AbstractRMQ {
         NaiveRMQ(uint32_t n, vector<uint64_t> &v) {
             rmq_solutions = vector<vector<uint32_t>>(n - 1);
 
-            for (uint32_t i = 0; i < v.size() - 1; i++) {
+            for (uint32_t i = 0; i < n - 1; i++) {
                 uint32_t cur_min_idx = i;
-                for (uint32_t j = i + 1; j < v.size(); j++) {
+                for (uint32_t j = i + 1; j < n; j++) {
                     if (v[j] < v[cur_min_idx]) {
                         cur_min_idx = j;
                     }
@@ -37,6 +38,109 @@ class NaiveRMQ : public AbstractRMQ {
         }
 };
 
+class LogLinearRMQ : public AbstractRMQ {
+    private:
+        vector<vector<uint32_t>> rmq_solutions;
+        vector<uint64_t> *v;
+
+        bool is_power_of_2(uint32_t n) {
+            return ((n & (n - 1)) == 0);
+        }
+    public:
+        LogLinearRMQ(uint32_t n, vector<uint64_t> &v) {
+            rmq_solutions = vector<vector<uint32_t>>(n);
+            this->v = &v;
+
+            for (uint32_t s = 0; s < n; s++) {
+                rmq_solutions[s].push_back(s);
+            }
+            
+            uint32_t idx1, idx2;
+
+            for (uint32_t l = 1; l <= log2(n); l++) {
+                for (uint32_t s = 0; s + pow(2, l) - 1 < n; s++) {
+                    idx1 = rmq_solutions[s][l - 1];
+                    idx2 = rmq_solutions[s + pow(2, l - 1)][l - 1];
+                    if (v[idx1] < v[idx2]) {
+                        rmq_solutions[s].push_back(idx1);
+                    } else {
+                        rmq_solutions[s].push_back(idx2);
+                    }
+                }
+            }
+        }
+
+        int rmq(uint32_t s, uint32_t e) {
+            if (is_power_of_2(e - s + 1)) {
+                uint32_t l = log2(e - s + 1);
+                return rmq_solutions[s][l];
+            }
+            uint32_t l = floor(log2(e - s - 1));
+
+            uint32_t idx1 = rmq_solutions[s][l];
+            uint32_t idx2 = rmq_solutions[e - pow(2, l) + 1][l];
+
+            if ((*v)[idx1] < (*v)[idx2]) {
+                return idx1;
+            } else {
+                return idx2;
+            }
+        }
+};
+
+void run_naive_rmq(ifstream &input_file, ofstream &output_file) {
+    vector<uint64_t> v;
+    uint32_t n;
+
+    input_file >> n;
+
+    uint64_t elem;
+    for (uint32_t i = 0; i < n; i++) {
+        input_file >> elem;
+        v.push_back(elem);
+    }
+
+    NaiveRMQ rmq_ds = NaiveRMQ(n, v);
+    v.clear();
+
+    uint32_t s, e;
+    string line;
+
+    while (input_file >> line) {
+        size_t comma_idx = line.find(','); 
+        uint32_t s = stoi(line.substr(0, comma_idx));
+        uint32_t e = stoi(line.substr(comma_idx + 1));
+
+        output_file << rmq_ds.rmq(s, e) << endl;
+    }
+}
+
+void run_loglinear_rmq(ifstream &input_file, ofstream &output_file) {
+    vector<uint64_t> v;
+    uint32_t n;
+
+    input_file >> n;
+
+    uint64_t elem;
+    for (uint32_t i = 0; i < n; i++) {
+        input_file >> elem;
+        v.push_back(elem);
+    }
+
+    LogLinearRMQ rmq_ds = LogLinearRMQ(n, v);
+
+    uint32_t s, e;
+    string line;
+
+    while (input_file >> line) {
+        size_t comma_idx = line.find(','); 
+        uint32_t s = stoi(line.substr(0, comma_idx));
+        uint32_t e = stoi(line.substr(comma_idx + 1));
+
+        output_file << rmq_ds.rmq(s, e) << endl;
+    }
+}
+
 int main(int argc, char* argv[]) {
 
     ifstream input_file (argv[2]);
@@ -51,31 +155,7 @@ int main(int argc, char* argv[]) {
     if (argv[1] == "pd"s) {
 
     } else if (argv[1] == "rmq"s) {
-        vector<uint64_t> v;
-        uint32_t n;
-
-        input_file >> n;
-
-        uint64_t elem;
-        for (uint32_t i = 0; i < n; i++) {
-            input_file >> elem;
-            v.push_back(elem);
-        }
-
-        NaiveRMQ rmq_ds = NaiveRMQ(n, v);
-        v.clear();
-
-        uint32_t s, e;
-        string line;
-
-        while (input_file >> line) {
-            size_t comma_idx = line.find(','); 
-            uint32_t s = stoi(line.substr(0, comma_idx));
-            uint32_t e = stoi(line.substr(comma_idx + 1));
-
-            output_file << rmq_ds.rmq(s, e) << endl;
-        }
-
+        run_loglinear_rmq(input_file, output_file);
     } else {
         throw invalid_argument("The first parameter must either be 'pd' or 'rmq'.");
     }
